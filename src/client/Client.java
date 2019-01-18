873a91cb9628944f7c61e4e0771cc83249e50f06
Yao Lee Teoh <yaoleeteoh@gmail.com>
2019-01-18 12:39:21 +0100

    created inputreader runnable

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,8 +1,6 @@
 package client;
 
-import sun.plugin2.message.Message;
-
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
@@ -24,9 +22,67 @@ public class Client {
             String userName = scanner.nextLine();
             ClientMessage helo = new ClientMessage(MessageType.HELO, userName);
 
+            //THREAD for reading input
+            InputStream is = socket.getInputStream();
+            InputReader ir = new InputReader(is);
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public class InputReader implements Runnable {
+        private BufferedReader reader;
+        private boolean isRunning;
+
+        public InputReader(InputStream is) {
+            this.reader = new BufferedReader(new InputStreamReader(is));
+            this.isRunning = true;
+        }
+
+        @Override
+        public void run() {
+
+            do {
+                try {
+                    String line = reader.readLine();
+                    line = line.contains(" ") ? line.split(" ")[0]: line;
+
+                    ClientMessage clientMessage;
+
+                    switch (line) {
+                        case "HELO":
+                            break;
+                        case "BCST":
+                            break;
+                        case "PING":
+                            break;
+                        case "USERS":
+                            break;
+                        case "DM":
+                            break;
+                        case "MKGROUP":
+                            break;
+                        case "GETGROUPS":
+                            break;
+                        case "JOIN":
+                            break;
+                        case "SHOUT":
+                            break;
+                        case "LEAVE":
+                            break;
+                        case "KICK":
+                            break;
+                        case "QUIT":
+                            // STOP HERE
+                            break;
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } while (this.isRunning);
         }
     }
 }
